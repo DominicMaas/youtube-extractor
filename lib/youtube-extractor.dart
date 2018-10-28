e976e207780e854b0b253acd4da2adb211df0656
Dominic Maas <dominic.maas@live.com>
2018-10-28 22:04:07 +1300

    Get the context

--- lib/youtube-extractor.dart
+++ lib/youtube-extractor.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:youtube_extractor/models/player-context.dart';
 
 /// Dart port of YouTubeExplode
@@ -9,17 +10,21 @@ class YouTubeExtractor {
     var url = 'https://www.youtube.com/embed/$videoId?disable_polymer=true&hl=en';
     var body = (await http.get(url)).body;
     
-    print(body);
+    // Extract the config part
+    var config = RegExp(r"yt\.setConfig\({'PLAYER_CONFIG':.+?\}\);", multiLine: true).firstMatch(body).group(0);
+    config = config.substring(30, config.length - 3);
 
+    // Decode the json
+    var root = json.decode(config);
+    
+    // Get the player source url
+    var playerSourceUrl = root["assets"]["js"].toString();
+    if (playerSourceUrl != null && playerSourceUrl.isNotEmpty) {
+      playerSourceUrl = "https://www.youtube.com" + playerSourceUrl;
+    }
 
-   
-    // Extract info
-    var playerSourceUrl = 'null'; // var relativeUrl = _root["assets"]["js"].Value<string>();
-    // if (relativeUrl.IsNotBlank())
-    // relativeUrl = "https://www.youtube.com" + relativeUrl;
-    // return relativeUrl;
-
-    var sts = 'null'; //public string ParseSts() => _root["sts"].Value<string>();
+    // Get the sts
+    var sts = root["sts"].toString();    
 
     // Check if successful
     if (playerSourceUrl == null || playerSourceUrl.isEmpty || sts == null || sts.isEmpty)
@@ -38,6 +43,11 @@ class YouTubeExtractor {
 
     // Get player context
     var playerContext = await _getVideoPlayerContextAsync(videoId);
+
+    // Get parser
+
+    print(playerContext.sts);
+    print(playerContext.sourceUrl);
   }
 
   /// Verifies that the given string is syntactically a valid YouTube video ID.
