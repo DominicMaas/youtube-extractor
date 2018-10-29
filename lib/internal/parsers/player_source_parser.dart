69dd4f7491542db8517f0f583a4ad37111f43e0a
Dominic Maas <dominic.maas@live.com>
2018-10-29 13:50:33 +1300

    It works, whoop!

--- lib/internal/parsers/player_source_parser.dart
+++ lib/internal/parsers/player_source_parser.dart
@@ -1,5 +1,8 @@
 import 'package:youtube_extractor/exceptions/parse_exception.dart';
 import 'package:youtube_extractor/internal/cipher_operations/base_cipher_operation.dart';
+import 'package:youtube_extractor/internal/cipher_operations/reverse_cipher_operation.dart';
+import 'package:youtube_extractor/internal/cipher_operations/slice_cipher_operation.dart';
+import 'package:youtube_extractor/internal/cipher_operations/swap_cipher_operation.dart';
 
 class PlayerSourceParser {
   String _raw;
@@ -20,7 +23,62 @@ class PlayerSourceParser {
     }
 
     // Find the body of the function
-    return List<BaseCipherOperation>();
+    var entryPointPattern = r"(?!h\.)" + RegExp.escape(entryPoint) + r"=function\(\w+\)\{(.*?)\}";
+    var entryPointBody = RegExp(entryPointPattern).firstMatch(_raw).group(1);
+    if (entryPointBody == null) {
+      throw ParseException('Could not find the signature decipherer function body.');
+    }
+    var entryPointLines = entryPointBody.split(';').toList();
+
+    // Identify cipher functions
+    String reverseFuncName = null;
+    String sliceFuncName = null;
+    String charSwapFuncName = null;
+    var operations = new List<BaseCipherOperation>();
+
+    // Analyze the function body to determine the names of cipher functions
+    for (var i = 0; i < entryPointLines.length; i++) {
+      // Break when all functions are found
+      if (reverseFuncName != null && sliceFuncName != null && charSwapFuncName != null) {
+        break;
+      }
+
+      // Get the function called on this line
+      var calledFuncName = RegExp(r"\w+\.(\w+)\(").firstMatch(entryPointLines[i]).group(1);
+      if (calledFuncName == null) {
+        continue;
+      }
+      
+      // Find cipher function names
+      if (RegExp(RegExp.escape(calledFuncName) + r":\bfunction\b\(\w+\)").hasMatch(_raw)) {
+        reverseFuncName = calledFuncName;
+      } else if (RegExp(RegExp.escape(calledFuncName) + r":\bfunction\b\([a],b\).(\breturn\b)?.?\w+\.").hasMatch(_raw)) {
+        sliceFuncName = calledFuncName;
+      } else if (RegExp(RegExp.escape(calledFuncName) + r":\bfunction\b\(\w+\,\w\).\bvar\b.\bc=a\b").hasMatch(_raw)) {
+        charSwapFuncName = calledFuncName;
+      }
+    }
+
+    // Analyze the function body again to determine the operation set and order
+    for (var i = 0; i < entryPointLines.length; i++) {
+      // Get the function called on this line
+      var calledFuncName = RegExp(r"\w+\.(\w+)\(").firstMatch(entryPointLines[i]).group(1);
+      if (calledFuncName == null) {
+        continue;
+      }
+   
+      if (calledFuncName == charSwapFuncName) { // Swap operation
+        var index = int.tryParse(RegExp(r"\(\w+,(\d+)\)").firstMatch(entryPointLines[i]).group(1));
+        operations.add(SwapCipherOperation(index));
+      } else if (calledFuncName == sliceFuncName) { // Slice operation
+        var index = int.tryParse(RegExp(r"\(\w+,(\d+)\)").firstMatch(entryPointLines[i]).group(1));
+        operations.add(SliceCipherOperation(index));
+      } else if (calledFuncName == reverseFuncName) { // Reverse operation
+        operations.add(ReverseCipherOperation());
+      }
+    }
+
+    return operations;
   }
 
   static PlayerSourceParser initialize(String raw) {
